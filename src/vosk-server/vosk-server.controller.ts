c3c31b3edd71ba9117cd63dfec109a742b804c4f
krasterisk <info@krasterisk.ru>
2026-02-21 15:17:44 +0700

    completely remove vosk-docker.service.ts to fix EAI_AGAIN vosk

--- src/vosk-server/vosk-server.controller.ts
+++ src/vosk-server/vosk-server.controller.ts
@@ -2,7 +2,6 @@ import { Controller, Post, UploadedFile, UseGuards, UseInterceptors } from "@nes
 import * as path from "path";
 import * as fs from "fs";
 import { VoskServerService } from "./vosk-server.service";
-import { VoskDockerService } from "./vosk-docker.service";
 import { FileInterceptor } from "@nestjs/platform-express";
 import { ApiResponse } from "@nestjs/swagger";
 import { Roles } from "../auth/roles-auth.decorator";
@@ -13,8 +12,7 @@ import { RolesGuard } from "../auth/roles.guard";
 export class VoskServerController {
 
     constructor(
-        private readonly voskService: VoskServerService,
-        private readonly voskDockerService: VoskDockerService
+        private readonly voskService: VoskServerService
     ) { }
 
     @Post('recognize')
@@ -38,21 +36,6 @@ export class VoskServerController {
         const audioBuffer = fs.readFileSync(fileName);
         const recognizedText = await this.voskService.processAudio(audioBuffer);
         fs.unlinkSync(fileName);
-        return recognizedText;
-    }
-
-    @Post('recognize-docker')
-    @ApiResponse({ status: 200, description: 'Speech recognition result from Docker Vosk container' })
-    @Roles('ADMIN')
-    @UseGuards(RolesGuard)
-    @UseInterceptors(FileInterceptor('audio'))
-    async recognizeSpeechDocker(@UploadedFile() file: Express.Multer.File): Promise<{ text: string }> {
-        if (!file) {
-            throw new Error('No file uploaded');
-        }
-
-        // We can pass the buffer directly to the new service without writing to disk
-        const recognizedText = await this.voskDockerService.processAudio(file.buffer);
         return recognizedText;
     }
 }
--- src/vosk-server/vosk-server.module.ts
+++ src/vosk-server/vosk-server.module.ts
@@ -1,13 +1,12 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { VoskServerService } from './vosk-server.service';
-import { VoskDockerService } from './vosk-docker.service';
 import { VoskServerController } from "./vosk-server.controller";
 import { AuthModule } from "../auth/auth.module";
 
 @Module({
   controllers: [VoskServerController],
-  providers: [VoskServerService, VoskDockerService],
-  exports: [VoskServerService, VoskDockerService],
+  providers: [VoskServerService],
+  exports: [VoskServerService],
   imports: [
     forwardRef(() => AuthModule)
   ],
